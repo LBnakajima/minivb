75fea0f0ac193402400d3f825da651a398b9e110
LB_Nakajima <nakazima@landbridge.vn>
2024-08-02 02:42:55 +0900

    Fix firebase import issue in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebaseConfig.js";
+import { db, auth, storage } from "../firebaseConfig.js";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
